12cb8935bb35bffca2a8d2ed31f547ea3201e2db
Issac Penn <pbbsignup@outlook.com>
2019-07-01 20:09:51 +0800

    Initial Commit

---
+++ Palette/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Palette
+//
+//  Created by Issac Penn on 2019/7/1.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
